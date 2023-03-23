68525d210b1766b6eee48eeb4fe0a726713a9554
Ayush Kumar Jha <ayush91176@gmail.com>
2023-03-23 10:18:22 +0530

    bhutki

--- crud/urls.py
+++ crud/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('insert/', StudentFormView.as_view(),name="insert"),
     path('<pk>/edit-student/', EditStudentView.as_view(),name="EditStudentView"),
     path('<pk>/delete-student/', DeleteStudentView.as_view(),name="DeleteStudentView"),
+    path('login-student/', LoginStudentView.as_view(),name="login"),
+    path('logout-student/', LogoutView.as_view(),name="logout"),
+    path('register/', SingupView.as_view(),name="SingupView"),
 ]
--- students/views.py
+++ students/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
 from .models import *
-from django.views.generic import ListView,FormView,DeleteView,CreateView,UpdateView
-# from .forms import *
-# def home(r):
-#     data={
-#         "student":StudentRecord.objects.all()
-#     }
-#     return render(r,"student_list.html",data)
+from django.views.generic import ListView,FormView,DeleteView,CreateView,UpdateView,View
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 class StudnetView(ListView):
     model = StudentRecord
-    # template_name="./home.html"
+    template_name="./home.html"
+    # def get(self, request):
+    #     if request.GET.get("search") != "":
+            
+    #     return super().get(request)
+    def get_queryset(self):
+        search=self.request.GET.get("search","")
+        return StudentRecord.objects.filter(name__icontains=search)
+    
 class StudentFormView(CreateView):
     template_name="./insert.html"
     model=StudentRecord
@@ -27,3 +33,33 @@ class EditStudentView(UpdateView):
     fields="__all__"
     success_url="/"
     template_name="insert.html"
+class LoginStudentView(FormView):
+    template_name="./login.html"
+    form_class=AuthenticationForm
+    success_url="/"
+    def post(self,req):
+        username=req.POST.get("username")
+        password=req.POST.get("password")
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            if user.is_active:
+                login(req,user)
+                return redirect("home")
+            else:  
+                return HttpResponse("inactivated")
+        else:
+            return HttpResponse("login cheeking is fail")
+class LogoutView(View):
+    def get(self,req):
+        logout(req)
+        return redirect("login")
+class SingupView(CreateView):
+    model=User
+    fields=["username","first_name","last_name","email","password"]
+    template_name="singup.html"
+    success_url="/login-student/"
+    def form_valid(self,form):
+        user =form.save(commit=False)
+        user.password=make_password(user.password)
+        user.save()
+        return  super().form_valid(form)
